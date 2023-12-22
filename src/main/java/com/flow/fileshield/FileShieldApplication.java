d599bc5da54f3a48062e579e68f98ac648191606
sa46lll <azure394@gmail.com>
2023-12-22 19:31:14 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/flow/fileshield/FileShieldApplication.java
@@ -0,0 +1,13 @@
+package com.flow.fileshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flow/fileshield/FileShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.fileshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
