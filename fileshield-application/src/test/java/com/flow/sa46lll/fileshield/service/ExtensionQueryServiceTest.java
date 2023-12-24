924238ed20614cda1f1011af700641b22b54fc4e
sa46lll <9812smh@naver.com>
2023-12-25 02:25:32 +0900

    test: ExtensionQuery 유즈케이스 단위 테스트 작성

---
+++ fileshield-application/src/test/java/com/flow/sa46lll/fileshield/service/ExtensionQueryServiceTest.java
@@ -0,0 +1,37 @@
+package com.flow.sa46lll.fileshield.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.flow.sa46lll.fileshield.domain.BlockedExtension;
+import com.flow.sa46lll.fileshield.domain.Extension;
+import com.flow.sa46lll.fileshield.domain.ExtensionType;
+import com.flow.sa46lll.fileshield.dto.GetExtensionsResponse;
+import com.flow.sa46lll.fileshield.port.out.ReadExtensionPersistencePort;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ExtensionQueryServiceTest {
+
+    @InjectMocks
+    private ExtensionQueryService sut;
+
+    @Mock
+    private ReadExtensionPersistencePort readExtensionPersistencePort;
+
+    @Test
+    void 확장자_목록을_조회한다() {
+        List<BlockedExtension> blockedExtensions = List.of(
+                new BlockedExtension(1L, Extension.of("bat"), ExtensionType.FIXED, true));
+
+        when(readExtensionPersistencePort.findAll()).thenReturn(blockedExtensions);
+
+        GetExtensionsResponse response = sut.findAll();
+        assertThat(response).isNotNull();
+    }
+}
