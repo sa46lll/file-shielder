ec8965bf03eb17e094e8e7992bc21cd1e522055f
sa46lll <9812smh@naver.com>
2023-12-25 00:15:07 +0900

    refactor: @transactional 추가

--- fileshield-application/src/main/java/com/flow/sa46lll/fileshield/service/ExtensionCommandService.java
+++ fileshield-application/src/main/java/com/flow/sa46lll/fileshield/service/ExtensionCommandService.java
@@ -11,8 +11,10 @@ import com.flow.sa46lll.fileshield.port.in.UpdateFixedExtensionUsecase;
 import com.flow.sa46lll.fileshield.port.in.UnblockCustomExtensionUseCase;
 import com.flow.sa46lll.fileshield.port.out.ExtensionPersistencePort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ExtensionCommandService implements BlockCustomExtensionUseCase,
         UnblockCustomExtensionUseCase, UpdateFixedExtensionUsecase {
 
--- fileshield-application/src/main/java/com/flow/sa46lll/fileshield/service/ExtensionQueryService.java
+++ fileshield-application/src/main/java/com/flow/sa46lll/fileshield/service/ExtensionQueryService.java
@@ -7,8 +7,10 @@ import com.flow.sa46lll.fileshield.port.in.GetExtensionQuery;
 import com.flow.sa46lll.fileshield.port.out.ExtensionPersistencePort;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class ExtensionQueryService implements GetExtensionQuery {
 
     private final ExtensionPersistencePort extensionPersistencePort;
